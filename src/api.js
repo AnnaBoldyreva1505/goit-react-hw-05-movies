5b7a9109e15d10ad7da0b750e84f0096113b4e79
AnnaBoldyreva1505 <hannabl1988@gmail.com>
2023-05-22 03:33:39 +0600

    add start files

---
+++ src/api.js
@@ -0,0 +1,4 @@
+// const API_Key = 'b6e9665514428b2c0f9442130a42f46a';
+// const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+
+// // Documentation: https://developers.themoviedb.org/3/getting-started/introduction
