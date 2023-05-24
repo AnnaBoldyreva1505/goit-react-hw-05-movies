ae6e51c766bed1bd846eeb2c025cef3453ad6f81
AnnaBoldyreva1505 <hannabl1988@gmail.com>
2023-05-25 01:19:15 +0600

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,23 @@
-// import { BrowserRouter } from "react-router-dom";
+import { Routes, Route, NavLink } from 'react-router-dom';
+import { AppBar } from './AppBar';
 
+export const App = () => {
+  return (
+    <>
+    <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="cats" element={<Cats />} />
+          <Route path="cats/:breedId" element={<CatDetails />} />
+        </Route>
+        <Route path="*" element={<div>Page not found</div>} />
+      </Routes> 
+    </>
+  );
+};
 
-
-// export const App = () => {
-//   return (
-//     <div>
-//     <Route path="/about" element={<About />} />
-//     </div>
-//   );
-// };
+// /trending/get-trending список самых популярных фильмов на сегодня для создания коллекции на главной странице.
+// /search/search-movies поиск кинофильма по ключевому слову на странице фильмов.
+// /movies/get-movie-details запрос полной информации о фильме для страницы кинофильма.
+// /movies/get-movie-credits запрос информации о актёрском составе для страницы кинофильма.
+// /movies/get-movie-reviews запрос обзоров для страницы кинофильма.
