15f14d0607359e8e2491f35c32fcbdf24cb4d86e
AnnaBoldyreva1505 <hannabl1988@gmail.com>
2023-05-29 02:32:18 +0600

    Update MovieDetails.js
    movie detalis jsx

---
+++ src/pages/MovieDetails/MovieDetails.js
@@ -0,0 +1,40 @@
+import {  } from "./MovieDetails.styled";
+
+const MovieDetails = () => {
+
+
+
+
+return (
+
+    <>
+    <img src='' alt=''/>
+    <h2>Movie Name</h2>
+    <p>User Score: </p>
+    <p>Overview </p>
+    <p>текст </p>
+    <p>Genres: </p>
+    <p>текст </p>
+
+    <div>
+    <p>Additional information</p>
+
+    <ul>
+        <li>
+            <a href="http://google.com">
+                Cast
+            </a>
+        </li>
+        <li>
+            <a href="http://google.com">
+                Reviews
+            </a>
+        </li>
+    </ul>
+    </div>
+    </>
+)
+
+}
+
+export default MovieDetails;
