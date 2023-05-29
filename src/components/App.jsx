9d440690ef58de9810e8d17b0dacb4fc3bf31d39
AnnaBoldyreva1505 <hannabl1988@gmail.com>
2023-05-29 19:38:58 +0600

    routs - done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,24 @@ import { Routes, Route } from 'react-router-dom';
 import { SharedLayout } from './SharedLayout';
 import Home from '../pages/Home/Home'
 import Movies from 'pages/Movies/Movies';
+import MovieDetails from 'pages/MovieDetails/MovieDetails';
+import Cast from './Cast/Cast';
+import Reviews from './Reviews/Reviews';
 
 export const App = () => {
   return (
     <>
-    <Routes>
-        <Route path="/" element={<SharedLayout />}>
+      <Routes>
+      <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home />} />
-          <Route path="movies" element={<Movies/>} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="/movies/:movie_id" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
         </Route>
-        <Route path="*" element={<div>Page not found</div>} />
-      </Routes> 
-
+        <Route path="*" element={<Home />} />
+      </Route>
+    </Routes>
     </>
   );
 };
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,1 +1,7 @@
-import styled from 'styled-components';
+
+
+const Reviews = () => {
+
+}
+
+export default Reviews
